83b249c3ed0b909883d8214b446d0cf21d20b036
Chandru2197 <55126754+Chandru2197@users.noreply.github.com>
2021-07-01 01:45:38 +0530

    jarvis jarvis

---
+++ src/Routing/routes.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Route></Route>
+      </Router>
+    );
+  }
+}
