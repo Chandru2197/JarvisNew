3266c3ef7fe273fe3e4c8bb699595c7c6af07536
Chandru2197 <55126754+Chandru2197@users.noreply.github.com>
2021-07-01 19:52:21 +0530

    Single Select

--- src/Screens/home.js
+++ src/Screens/home.js
@@ -8,8 +8,45 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <div className="alert alert-primary" role="alert">
+        <div class="alert alert-primary" role="alert">
           This is a primary alert—check it out!
+        </div>
+        <br />
+        <div class="card" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div class="dropdown">
+              <button
+                class="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown button
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </div>
+            <a href="#" class="card-link">
+              Another link
+            </a>
+          </div>
         </div>
       </div>
     );
